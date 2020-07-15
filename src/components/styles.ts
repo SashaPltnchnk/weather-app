018bf692f69b37b02107738a04215038466e7278
Zi <ZDubinaQ@gmail.com>
2020-07-15 13:58:40 +0300

    feat(components): create bottom tab navigator

---
+++ src/components/styles.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  tab: {
+    paddingBottom: 15,
+  },
+});
