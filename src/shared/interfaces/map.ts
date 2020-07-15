a09162397941753cb97029958bf8a931673b7beb
Zi <ZDubinaQ@gmail.com>
2020-07-15 13:54:50 +0300

    feat(shared:interfaces): created interface for map

---
+++ src/shared/interfaces/map.ts
@@ -0,0 +1,11 @@
+export interface Coordinates {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
+
+export interface Address {
+  city: string;
+  country: string;
+}
