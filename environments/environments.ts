a848c7a227c0c8ba952c3ea27336a278d9e610dc
Zi <ZDubinaQ@gmail.com>
2020-07-15 13:55:28 +0300

    build(environments): created common file for keys

---
+++ environments/environments.ts
@@ -0,0 +1,5 @@
+export const Config = {
+  GOOGLE_API_KEY: 'AIzaSyBR7XyYYSTbUxiOlTz03lEoqsJZuRIBSeg',
+  WEATHER_API_KEY: 'e0acb4ac3794e713f302e4aecdab3a1a',
+  // WEATHER_API_KEY: '4f185442f39cb60bddd3797e905a42fa',
+};
